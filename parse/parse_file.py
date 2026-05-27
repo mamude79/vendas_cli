727083b8a23ac0c5129f6da3e7642329a8ee096d
Samir Mamude <mamude@gmail.com>
2026-05-27 15:49:36 -0300

    handle invalid csv format

--- parse/parse_file.py
+++ parse/parse_file.py
@@ -14,46 +14,49 @@ CHUNK_SIZE: int = 1000
 
 
 def read_data(args: argparse.Namespace) -> None:
-    rows: list = []
-    total_sales: float = 0.0
-    product_totals = defaultdict(int)
+    try:
+        rows: list = []
+        total_sales: float = 0.0
+        product_totals = defaultdict(int)
 
-    for data in pandas.read_csv(args.input, dtype=SCHEMA, chunksize=CHUNK_SIZE):
-        for _, row in data.iterrows():
-            if args.start and args.end:
-                start_date = pandas.to_datetime(args.start)
-                end_date = pandas.to_datetime(args.end)
-                formatted_date = pandas.to_datetime(
-                    row["date"], dayfirst=False, errors="coerce"
-                )
+        for data in pandas.read_csv(args.input, dtype=SCHEMA, chunksize=CHUNK_SIZE):
+            for _, row in data.iterrows():
+                if args.start and args.end:
+                    start_date = pandas.to_datetime(args.start)
+                    end_date = pandas.to_datetime(args.end)
+                    formatted_date = pandas.to_datetime(
+                        row["date"], dayfirst=False, errors="coerce"
+                    )
 
-                if formatted_date >= start_date and formatted_date <= end_date:
+                    if formatted_date >= start_date and formatted_date <= end_date:
+                        row = _add_calculations(row)
+                        total_sales += _sum_total_sales(row)
+                        _get_product_groupby_sales(row, product_totals)
+                        rows.append(row)
+                else:
                     row = _add_calculations(row)
                     total_sales += _sum_total_sales(row)
                     _get_product_groupby_sales(row, product_totals)
                     rows.append(row)
-            else:
-                row = _add_calculations(row)
-                total_sales += _sum_total_sales(row)
-                _get_product_groupby_sales(row, product_totals)
-                rows.append(row)
 
-    # exibir resultado no formato selecionado
-    if args.format == "text":
-        table.print_table(rows)
-    else:
-        json.print_json(rows)
+        # exibir resultado no formato selecionado
+        if args.format == "text":
+            table.print_table(rows)
+        else:
+            json.print_json(rows)
 
-    # Informa o valor total de todas as vendas
-    logger.info(
-        "Valor total de todas as vendas: {}".format(format_currency(total_sales, "BRL"))
-    )
-    # Ordena e pegar o produto mais vendido
-    sorted_products = sorted(product_totals.items(), key=lambda x: x[1], reverse=True)
-    best_product = sorted_products[0][0] if sorted_products else ""
-    max_quantity = sorted_products[0][1] if sorted_products else 0
+        # Informa o valor total de todas as vendas
+        logger.info(
+            "Valor total de todas as vendas: {}".format(format_currency(total_sales, "BRL"))
+        )
+        # Ordena e pegar o produto mais vendido
+        sorted_products = sorted(product_totals.items(), key=lambda x: x[1], reverse=True)
+        best_product = sorted_products[0][0] if sorted_products else ""
+        max_quantity = sorted_products[0][1] if sorted_products else 0
 
-    logger.info("Produto mais vendido: {} - {}".format(best_product, max_quantity))
+        logger.info("Produto mais vendido: {} - {}".format(best_product, max_quantity))
+    except (Exception, AttributeError):
+        logger.error("### FORMATO DO CSV INVÁLIDO!!! ###")
 
 
 def _add_calculations(row: Series) -> Series:
