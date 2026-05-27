bf54e7dbbc5e5a6dd1a783fbe9282820354011a0
Samir Mamude <mamude@gmail.com>
2026-05-27 14:11:28 -0300

    added product group by sale

--- parse/parse_file.py
+++ parse/parse_file.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from collections import defaultdict
 from functools import total_ordering
 
 import pandas
@@ -18,8 +19,9 @@ CHUNK_SIZE: int = 1000
 def read_data(args: argparse.Namespace) -> None:
     rows: list = []
     total_sales: float = 0.0
-    for data in pandas.read_csv(args.input, dtype=SCHEMA, chunksize=CHUNK_SIZE):
+    product_totals = defaultdict(int)
 
+    for data in pandas.read_csv(args.input, dtype=SCHEMA, chunksize=CHUNK_SIZE):
         for _, row in data.iterrows():
             if args.start and args.end:
                 start_date = pandas.to_datetime(args.start)
@@ -30,9 +32,13 @@ def read_data(args: argparse.Namespace) -> None:
 
                 if formatted_date >= start_date and formatted_date <= end_date:
                     row = _add_calculations(row)
-                    total_sales += float(row.price) * row.quantity
+                    total_sales += _sum_total_sales(row)
+                    _get_product_groupby_sales(row, product_totals)
                     rows.append(row)
             else:
+                row = _add_calculations(row)
+                total_sales += _sum_total_sales(row)
+                _get_product_groupby_sales(row, product_totals)
                 rows.append(row)
 
     # exibir resultado no formato selecionado
@@ -43,8 +49,24 @@ def read_data(args: argparse.Namespace) -> None:
 
     # Informa o valor total de todas as vendas
     logger.info("Valor total de todas as vendas: {}".format(format_currency(total_sales, "BRL")))
+    # Ordena e pegar o produto mais vendido
+    sorted_products = sorted(product_totals.items(), key=lambda x: x[1], reverse=True)
+    best_product = sorted_products[0][0] if sorted_products else ""
+    max_quantity = sorted_products[0][1] if sorted_products else 0
+
+    logger.info("Produto mais vendido: {} - {}".format(best_product, max_quantity))
 
 def _add_calculations(row: Series) -> Series:
     row["current_price"] = format_currency(row.price, "BRL")
     row["total_sales"] = format_currency(float(row.price) * row.quantity, "BRL")
     return row
+
+def _sum_total_sales(row: Series) -> float:
+    return float(row.price) * row.quantity
+
+def _get_product_groupby_sales(row: Series, product_totals: dict):
+    if row["quantity"] > 0 and pandas.notna(row["quantity"]):
+        product = row["product"]
+        current_total = product_totals.get(product, 0)
+        product_totals[product] = current_total + int(row['quantity'])
+        return product_totals
